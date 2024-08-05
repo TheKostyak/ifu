471b32b9da0b71e9e95da52be95644aad8066f1f
Kostya <TheKostyak@users.noreply.github.com>
2024-08-06 00:43:57 +0200

    added courses schedules block

---
+++ courses-schedule/dropdown.js
@@ -0,0 +1,17 @@
+const dropdown = document.getElementsByClassName("courses-list__item-dropdown");
+let i;
+console.dir(dropdown)
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+    console.dir(this)
+    console.dir(this.nextElementSibling)
+    this.classList.toggle("dropdown-active");
+    var dropdownContent = this.children[0];
+    console.dir(dropdownContent)
+    if (dropdownContent.style.display === "flex") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "flex";
+    }
+  });
+}
